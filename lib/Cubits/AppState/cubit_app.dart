63aab72c07b2833086e0a7fd8652dda128e823d9
Taosif7 <taosif.7@gmail.com>
2021-04-01 21:41:16 +0530

    Added AppState methods to SignIn or SignOut user

--- lib/Cubits/AppState/cubit_app.dart
+++ lib/Cubits/AppState/cubit_app.dart
@@ -1,6 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:link_ring/API/Models/model_group.dart';
+import 'package:link_ring/API/Models/model_user.dart';
 import 'package:link_ring/Cubits/AppState/state_app.dart';
 
 class cubit_app extends Cubit<state_app> {
   cubit_app(state_app initialState) : super(initialState);
+
+  void setLoggedInState(model_user user, List<model_group> joinedGroups) {
+    emit(state.copyWith(isLoggedIn: true, joinedGroups: joinedGroups, user: user));
+  }
+
+  void signOut() {
+    emit(new state_app(isLoggedIn: false));
+  }
 }
--- lib/Cubits/AppState/state_app.dart
+++ lib/Cubits/AppState/state_app.dart
@@ -1,10 +1,19 @@
+import 'package:link_ring/API/Models/model_group.dart';
+import 'package:link_ring/API/Models/model_user.dart';
+
 class state_app {
   bool isLoggedIn = false;
+  model_user currentUser;
+  List<model_group> groups = [];
 
-  state_app({bool this.isLoggedIn});
+  state_app({this.isLoggedIn, this.groups = const [], this.currentUser});
 
-  state_app copyWith({bool isLoggedIn}) {
-    state_app newState = new state_app(isLoggedIn: isLoggedIn ?? this.isLoggedIn);
+  state_app copyWith({bool isLoggedIn, List<model_group> joinedGroups, model_user user}) {
+    state_app newState = new state_app(
+      isLoggedIn: isLoggedIn ?? this.isLoggedIn,
+      groups: joinedGroups ?? this.groups,
+      currentUser: user ?? this.currentUser,
+    );
     return newState;
   }
 }
