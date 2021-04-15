41e1c23c98ce8898ae7f6796c51da88180e83aca
Taosif7 <taosif.7@gmail.com>
2021-04-15 16:13:01 +0530

    Added Regex Patterns Utility class Improved Circle Avatar Widget to support emojis

--- lib/Screens/Commons/ProfileCircleAvatar.dart
+++ lib/Screens/Commons/ProfileCircleAvatar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:link_ring/API/Models/model_member.dart';
 import 'package:link_ring/API/Models/model_user.dart';
+import 'package:link_ring/Utils/RegexPatterns.dart';
 
 class ProfileCircleAvatar extends StatelessWidget {
   model_member member;
@@ -15,17 +16,22 @@ class ProfileCircleAvatar extends StatelessWidget {
 
   ProfileCircleAvatar.fromText(this.text, {this.size = 20}) {
     this.text = this.text.trim();
+    if (RegexPatterns.emojis.firstMatch(this.text) != null)
+      this.text = RegexPatterns.emojis.stringMatch(this.text);
+    else if (this.text.length >= 2) {
+      String finalText = text.substring(0, 2);
+      if (text.contains(" "))
+        finalText = text
+            .split(" ")
+            .reduce((value, element) => value.substring(0, 1).toUpperCase() + element.substring(0, 1).toUpperCase());
+      text = finalText;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     if (text != null && text.length >= 2) {
-      String label = text.substring(0, 2);
-      if (text.contains(" "))
-        label = text
-            .split(" ")
-            .reduce((value, element) => value.substring(0, 1).toUpperCase() + element.substring(0, 1).toUpperCase());
-      return CircleAvatar(child: Text(label), maxRadius: size);
+      return CircleAvatar(child: Text(text), maxRadius: size);
     } else if (user != null) {
       return CircleAvatar(backgroundImage: NetworkImage(user.profile_pic_url), maxRadius: size);
     } else if (member != null) {
