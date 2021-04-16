860444b73790225f66215f26e5f30973caa3d093
Taosif7 <Taosif.7@gmail.com>
2021-04-16 16:56:16 +0530

    Theme Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:link_ring/Cubits/AppState/cubit_app.dart';
 import 'package:link_ring/Screens/HomeScreen/Homepage.dart';
@@ -8,6 +9,11 @@ import 'package:link_ring/Screens/SignInScreen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // transparent status bar
+      statusBarIconBrightness: Brightness.dark // dark text for status bar
+      ));
 
   runApp(BlocProvider<cubit_app>(create: (c) => cubit_app(), child: MyApp()));
 }
@@ -20,9 +26,27 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.green,
           scaffoldBackgroundColor: Colors.blueGrey.shade50,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: UnderlineInputBorder(
+                  borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.transparent, width: 2)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.blueGrey, width: 2)),
+              errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.red.shade800, width: 2)),
+              focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10), borderSide: BorderSide(color: Colors.red.shade600, width: 2)),
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              filled: true,
+              fillColor: Colors.white),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              elevation: 0,
+              hoverElevation: 0,
+              highlightElevation: 0),
           appBarTheme: AppBarTheme(
               color: Colors.white,
               elevation: 0,
+              iconTheme: IconThemeData(color: Colors.blueGrey),
               textTheme: TextTheme(headline6: TextStyle(color: Colors.blueGrey, fontSize: 26, fontWeight: FontWeight.bold)))),
       home: context.read<cubit_app>().auth.state.isLoggedIn ? Screen_HomePage() : Screen_SignIn(),
     );
