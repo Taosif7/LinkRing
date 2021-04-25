2ecf9377809f73314d4ddb5c444c9827f50d2a21
Taosif7 <Taosif.7@gmail.com>
2021-04-25 17:08:42 +0530

    Improvements to link model New callNotification model that encapsulates all entities for call notifications

---
+++ android/app/src/main/java/com/taosif7/link_ring/models/model_callNotification.java
@@ -0,0 +1,22 @@
+package com.taosif7.link_ring.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class model_callNotification {
+
+    public model_link link;
+    public model_group group;
+    public model_member sender;
+
+    public model_callNotification(JSONObject data) {
+        try {
+            this.link = new model_link(data.getJSONObject("link"));
+            this.group = new model_group(data.getJSONObject("group"));
+            this.sender = new model_member(data.getJSONObject("sender"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- android/app/src/main/java/com/taosif7/link_ring/models/model_link.java
+++ android/app/src/main/java/com/taosif7/link_ring/models/model_link.java
@@ -1,5 +1,7 @@
 package com.taosif7.link_ring.models;
 
+import android.net.Uri;
+
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.json.JSONObject;
@@ -7,7 +9,9 @@ import org.json.JSONObject;
 public class model_link {
 
     public String id, link, name, senderId;
-    DateTime sentTime;
+    public DateTime sentTime;
+    public Uri uri;
+    public boolean hasName;
 
     public model_link(JSONObject object) {
         this.id = object.optString("id");
@@ -15,6 +19,8 @@ public class model_link {
         this.name = object.optString("name");
         this.senderId = object.optString("sent_by");
         this.sentTime = DateTime.parse(object.optString("sent_time"), DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSSSSSZ"));
+        this.uri = Uri.parse(this.link);
+        this.hasName = this.name.length() > 0 && !this.name.equals("null");
     }
 
 }
