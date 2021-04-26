820b8bd9456ba7b8399435ed79eb16d2a83e5ec2
Taosif7 <taosif.7@gmail.com>
2021-04-27 01:29:50 +0530

    Added API Method to send links

--- lib/API/Services/service_links.dart
+++ lib/API/Services/service_links.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 import 'package:link_ring/API/Models/model_group.dart';
 import 'package:link_ring/API/Models/model_link.dart';
+import 'package:link_ring/Utils/Constants.dart';
 
 class service_links {
+  static const API_ENDPOINT_SENDLINK = "https://linkring.herokuapp.com/ringlink";
+
   static service_links _service_instance;
   FirebaseFirestore firestore;
 
@@ -56,5 +62,23 @@ class service_links {
 
     snapshot.docs.forEach((element) => links.add(new model_link.fromMap(element.data())));
     return links;
+  }
+
+  Future<bool> sendLink(String groupId, String senderId, String link, String linkTitle) async {
+    http.Response response = await http.post(
+      Uri.parse(API_ENDPOINT_SENDLINK),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + SERVER_API_SECRET,
+      },
+      body: jsonEncode({
+        "link_name": linkTitle,
+        "link": link,
+        "groupId": groupId,
+        "senderId": senderId,
+      }),
+    );
+
+    return response.statusCode == 200;
   }
 }
---
+++ lib/Utils/Constants.dart
@@ -0,0 +1,1 @@
+const SERVER_API_SECRET = "KYw8m7c0kpIpvwd6L0ytcXHx3AEMD24nxPSpCPEs";
